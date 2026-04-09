6591f24748b5ba2bfe516ceb327ee9b6ecdf00f2
Mael CAETANO <mael.caetano@ynov.com>
2026-04-09 10:18:00 +0200

    FileSearch

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
-
 public class ExerciseRunner {
-    public static void main(String[] args) throws IOException {
-        FileManager.createFile("file.txt", "Lorem ipsum");
-        System.out.println(FileManager.getContentFile("file.txt"));
-        FileManager.deleteFile("file.txt");
+    public static void main(String[] args) {
+        String result = FileSearch.searchFile("file.txt");
+        if (result != null) {
+            System.out.println(result.replace("\\", "/")); // Force le slash pour le format attendu
+        }
     }
 }
